15834b4a34bd8898b7c6cb65c890d34717e3e42b
mikiasgebre <mikiasgebre@gmail.com>
2017-11-02 21:52:23 +0200

    Project Done!

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.londonappbrewery.bitcointicker;
 
+import android.icu.util.Currency;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -10,29 +11,49 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
-
-
+import com.loopj.android.http.*;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
-public class MainActivity extends AppCompatActivity {
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+import static android.R.attr.numberPickerStyle;
+import static android.R.attr.value;
+import static android.R.id.message;
+import static android.provider.UserDictionary.Words.APP_ID;
+import static com.londonappbrewery.bitcointicker.R.id.currency_spinner;
+import static org.json.JSONObject.*;
+
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    private String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC";
 
     // Member Variables:
     TextView mPriceTextView;
+    Spinner spinner;
+    String currency;
+    String BASE_URL2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mPriceTextView = (TextView) findViewById(R.id.priceLabel);
-        Spinner spinner = (Spinner) findViewById(R.id.currency_spinner);
+        spinner = (Spinner) findViewById(currency_spinner);
 
         // Create an ArrayAdapter using the String array and a spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -43,37 +64,70 @@ public class MainActivity extends AppCompatActivity {
 
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-
-        // TODO: Set an OnItemSelected listener on the spinner
-
+        spinner.setOnItemSelectedListener(this);
     }
 
-    // TODO: complete the letsDoSomeNetworking() method
-    private void letsDoSomeNetworking(String url) {
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+    // TODO: Set an OnItemSelected listener on the spinner
+
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        // An item was selected. You can retrieve the selected item using
+        parent.getItemAtPosition(pos);
+        //convert to string
+        currency = String.valueOf(parent.getItemAtPosition(pos));
+        BASE_URL2 = BASE_URL + currency;
+        letsDoSomeNetworking();
+        Toast.makeText(this, "You selected " + currency, Toast.LENGTH_SHORT).show();
+
 
 
     }
 
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+
+    }
+
+
+
+    //TODO: complete the letsDoSomeNetworking() method
+    private void letsDoSomeNetworking() {
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(BASE_URL2, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                //called before request is started
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                // called when response HTTP status is "200 OK"
+
+                try {
+                    currency = response.getString("ask");
+                    mPriceTextView.setText(currency);
+                } catch (JSONException e)
+                {
+                    e.printStackTrace();
+                    return ;
+                }
+                }
+
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String fe, Throwable e) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+
+                Toast.makeText(MainActivity.this, "Request Failed", Toast.LENGTH_SHORT).show();
+
+
+            }
+
+        });
+
+    }
 
 }
